477f8aa799a237a28678153ccb0b6755e18976de
Carlton Agesa Mbaja <65759360+carlagesa@users.noreply.github.com>
2022-06-23 22:41:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // Write your solution in this file!
 
-var employee = {
-  name: "carl",
-  streetAddress: "20 Nairobi",
+const employee = {
+    name: "Carlton",
+    streetAddress: "20 Kimathi Street",
 };
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-  let newEmployee = { ...employee };
-  newEmployee[key] = value;
-  return newEmployee;
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, { [key]: value});
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-  employee[key] = value;
-  return employee;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
 }
-function deleteFromEmployeeByKey(employee, key) {
-  let newEmployee = { ...employee };
-  delete newEmployee[key];
-  return newEmployee;
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;
 }
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-  delete employee[key];
-  return employee;
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+
+    return employee;
 }
